56e39250e1fb5b5851da561fdc36e169ea290b4d
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2025-09-05 03:31:40 +0900

    Calculate electric scalar potential

--- 2025/magstat-wire/wire.py
+++ 2025/magstat-wire/wire.py
@@ -25,8 +25,24 @@ print(mesh.ne, mesh.nv, mesh.GetMaterials(), mesh.GetBoundaries())
 
 material_cf = CoefficientFunction([1, 2])  # coil=1, air=2
 
+fespot = H1(mesh, order=2, definedon=mesh.Materials("coil"), dirichlet="out")
+phi,psi = fespot.TnT()
+sigma = 58.7e6
+with TaskManager():
+    bfa = BilinearForm(sigma*grad(phi)*grad(psi)*dx+1e-6*sigma*phi*psi*dx).Assemble()
+    inv = bfa.mat.Inverse(freedofs=fespot.FreeDofs(), inverse="sparsecholesky")
+    lff = LinearForm(1/crosssection*psi*ds("in")).Assemble()
+    gfphi = GridFunction(fespot)
+    gfphi.vec.data = inv * lff.vec
+
+fesL2 = VectorL2(mesh, order=1, definedon=mesh.Materials("coil"))
+gfJ = GridFunction(fesL2)
+gfJ = -sigma*grad(gfphi)
+
 pairs = [
     (material_cf, "MaterialID"),
+    (gfphi, "electr.scala.pot."),
+    (gfJ, "J"),
 ]
 
 vtk = VTKOutput(ma=mesh,
